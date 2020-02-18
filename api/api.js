e989366592431f422394950f8ad61774409edf35
issaafalkattan <issaaf.kattan@cognite.com>
2020-02-18 15:44:19 +0100

    IF I GOT ANY COOLER I WOULD FREEZE TO DEATH

--- api/api.js
+++ api/api.js
@@ -1,34 +1,9 @@
-const unsplashAccessKey = process.env.UNSPLASH_KEY;
-var keyword_extractor = require("keyword-extractor");
 
-console.log(process.env)
 
 export const getKanyefied = quote => {
-  let keywords = keyword_extractor.extract(quote);
-  return fetch(
-    `https://api.unsplash.com/photos/random?query=${keywords[0]}&orientation=landscape`,
-    { headers: { Authorization: `Client-ID ${unsplashAccessKey}` } }
-  )
-    .then(response => {
-      return response.json();
-    })
-    .then(myJson => {
-        console.log(myJson);
-      return {url : myJson.urls.full, color : myJson.color, user : myJson.user.name};
-    }).catch(() =>   {
 
-      return {
-        url: './roolbackImage.jpg', user: 'Cognite AS'
-      }
-    })
 };
 
 export const getQuote = () => {
-  return fetch("https://api.kanye.rest?format=json")
-    .then(response => {
-      return response.json();
-    })
-    .then(myJson => {
-      return myJson.quote;
-    });
+  
 };
--- pages/index.js
+++ pages/index.js
@@ -27,9 +27,6 @@ const Home = () => {
     });
   };
 
-  useEffect(() => {
-    handleKanye();
-  }, []);
 
   if(shareWindowVisible) 
   return (
